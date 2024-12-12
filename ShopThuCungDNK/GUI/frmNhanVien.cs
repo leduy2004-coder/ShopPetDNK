22574196c18284d5103d8efa8d67e0218c1b60c9
leduy2004-coder <leduy8762@gmail.com>
2024-12-12 23:21:56 +0700

    first commit

---
+++ ShopThuCungDNK/GUI/frmNhanVien.cs
@@ -0,0 +1,64 @@
+﻿using QuanLySieuThi.Class;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShopThuCungDNK.GUI
+{
+    public partial class frmNhanVien : Form
+    {
+        Main M = new Main();
+        public frmNhanVien()
+        {
+            InitializeComponent();
+        }
+        public static string tenDNMain = "";
+        public static string maNVMain = "";
+        void ThongTinDangNhap()
+        {
+            lb_Name.Text = tenDNMain;
+            //lblTen.Text = M.HoTen(tenDNMain);
+        }
+        private void frmNhanVien_Load(object sender, EventArgs e)
+        {
+            ThongTinDangNhap();
+        }
+
+        private void btnHome_Click_1(object sender, EventArgs e)
+        {
+            frmNVTrangChu frmNVTrangChu = new frmNVTrangChu();
+            frmNVTrangChu.TopLevel = false;
+
+            if (panelDesktop.Controls.Count > 0)
+            {
+                panelDesktop.Controls.Clear();
+            }
+
+            // Đặt kích thước của frmNVTrangChu bằng kích thước của panelDesktop
+            frmNVTrangChu.Size = panelDesktop.ClientSize;
+
+    
+            panelDesktop.Controls.Add(frmNVTrangChu);
+
+            // Đảm bảo frmNVTrangChu hiển thị lên trên cùng
+            frmNVTrangChu.BringToFront();
+
+            frmNVTrangChu.Show();
+        }
+
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+           
+            this.Hide();
+            frmDangNhap frmDangNhap = new frmDangNhap();
+            frmDangNhap.Show();
+        }
+    }
+}
---
+++ ShopThuCungDNK/GUI/frmQuanLy.cs
@@ -0,0 +1,41 @@
+﻿using QuanLySieuThi.Class;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShopThuCungDNK.GUI
+{
+    public partial class frmQuanLy : Form
+    {
+        Main M = new Main();
+
+        public static string tenDNMain = "";
+        public static string maNVMain = "";
+
+        public frmQuanLy()
+        {
+            InitializeComponent();
+        }
+        void ThongTinDangNhap()
+        {
+            lb_Name.Text = tenDNMain;
+        }
+        
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmQuanLy_Load(object sender, EventArgs e)
+        {
+            ThongTinDangNhap();
+        }
+    }
+}
